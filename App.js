04fe3cb78fffb69a3798c9edab9f0a57e8fc3875
chinmay <chinmaymhatre12@gmail.com>
2021-12-29 13:51:32 +0530

    api complete

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { H1, Button, NativeBaseProvider } from "native-base";
+import User from "./User.js";
+import axios from "axios";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [details, setDetails] = useState("");
+    const [username, setusername] = useState("");
+
+    const getData = async (username) => {
+        try {
+            let url = `https://api.github.com/users/${username.toLowerCase()}`;
+            const { data } = await axios.get(url);
+            setDetails(data);
+            setusername("");
+        } catch {
+            setDetails("Not Found");
+            setusername("");
+        }
+    };
+    return (
+        <NativeBaseProvider>
+            <View style={styles.container}>
+                <Text style={styles.heading}>GitHub API</Text>
+                <View style={styles.innerContainer}>
+                    {details ? (
+                        details == "Not Found" ? (
+                            <Text style={styles.error}>User Not Found</Text>
+                        ) : (
+                            <User details={details} />
+                        )
+                    ) : (
+                        <></>
+                    )}
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(text) => setusername(text)}
+                        placeholder="Enter your github username"
+                        value={username}
+                        placeholderTextColor="#fff"
+                    />
+                    <Button
+                        success
+                        style={styles.button}
+                        onPress={() => getData(username)}
+                    >
+                        <Text style={{ color: "#fff", fontWeight: "bold" }}>
+                            Submit
+                        </Text>
+                    </Button>
+                </View>
+            </View>
+        </NativeBaseProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#0C1017",
+    },
+    error: {
+        fontSize: 30,
+        color: "#fff",
+        paddingBottom: 200,
+    },
+    input: {
+        borderColor: "#292E34",
+        borderWidth: 1,
+        padding: 10,
+        marginTop: 20,
+        width: "70%",
+        borderRadius: 10,
+        textAlign: "center",
+        marginBottom: 20,
+        color: "#fff",
+    },
+    innerContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    heading: {
+        color: "#fff",
+        textAlign: "center",
+        padding: 20,
+        marginTop: 50,
+        fontSize: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: "#292E34",
+    },
+    button: {
+        padding: 50,
+        alignSelf: "center",
+        borderRadius: 10,
+        marginTop: 30,
+        width: "40%",
+    },
 });
